e8e4045113f104c0cf7892d3668c6b6db156a502
Riley <ryry1002yt@gmail.com>
2025-10-21 18:44:58 +1100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as modlib from './modlib';
+
+let init = false;
+
+function helloWorld(): void {
+	// you can view these logs at 
+	// %LOCALAPPDATA%\Temp\Battlefieldâ„¢ 6
+	console.log("Hello World")
+}
+
+
+export function OngoingGlobal() {
+	if (!init) {
+		helloWorld();
+	}
+	init = true;
+}
+
